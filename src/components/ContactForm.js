b0a5d27d5cdb54bcf2559d10e22585077a4dbef4
Lukhasz <lukaskaarle@gmail.com>
2023-11-05 20:25:30 +0100

    Added functionality in the ContactForm, it now rejects non-valid submissions. Also added some styling to ContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ContactForm = () => {
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  useEffect(() => {
+    const isFormValid = Object.values(form).every((value) => value !== "");
+    setIsFormValid(isFormValid);
+  }, [form]);
+
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isFormValid) {
+      setIsSubmitted(true);
+    } else {
+      alert("Please fill in all fields.");
+    }
+  };
+
   return (
     <div className="contact-us" id="contact">
       <h2>Contact Me!</h2>
-      <p>If you have any questions, please contact me here.</p>
+      {isSubmitted ? 
+        <p style={{color: 'green'}}>Thank you for contacting me, I will be in touch.</p> : 
+        <p>If you have any questions, please contact me here.</p>
+      }
 
-      <form className="contact-form">
-        <input type="text" placeholder="Name" required />
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
 
-        <input type="email" placeholder="Email" required />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
         
-        <textarea className="contact-textarea" placeholder="Message" rows={5} required></textarea>
+        <textarea className="contact-textarea" name="message" placeholder="Message" rows={5} onChange={handleChange} required></textarea>
         
-        <button type="submit" onClick={() => alert("Thank you for contacting me, I will be in touch.")}>Send</button>
+        <button type="submit">Send</button>
       </form>
 
     </div>
