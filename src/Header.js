6a2f0c11a8154ceed4318df7f1bf18303e194c0d
Lukhasz <lukaskaarle@gmail.com>
2023-11-06 09:30:39 +0100

    updated header

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 const Header = () => {
+
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
+    const y = element.getBoundingClientRect().top + window.pageYOffset - 20; // 20 is the offset
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  };
+  
   return (
     <header>
       <nav className='header-nav'>
         <ul className='header-list'>
-          <li><a href="#hero-section">Home</a></li>
-          <li><a href="#about-me">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a onClick={() => scrollTo('hero-section')}>Home</a></li>
+          <li><a onClick={() => scrollTo('about-me')}>About</a></li>
+          <li><a onClick={() => scrollTo('projects')}>Projects</a></li>
+          <li><a onClick={() => scrollTo('contact')}>Contact</a></li>
         </ul>
       </nav>
     </header>
   );
 };
+
+
+
 
 export default Header;
